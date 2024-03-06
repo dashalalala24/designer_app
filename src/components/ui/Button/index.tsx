6d119404cb5f16a8aa753f2953c26e71753d6304
Daria Lazarchuk <dashal_O_o@mail.ru>
2024-03-07 02:34:13 +0300

    try HOC

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,23 +1,36 @@
-import { CSSProperties, FC } from 'react';
+import { CSSProperties, FC, SVGProps } from 'react';
 import styles from './styles.module.css';
+import SvgIcon from '../SvgIcon/SvgIcon';
 
 type ButtonProps = {
   text: string;
   style?: CSSProperties;
   icon?: string;
-  iconStyle?: CSSProperties;
+  iconStyle?: SVGProps<SVGSVGElement>;
   variant?: 'smooth' | 'default';
 };
 
-export const Button: FC<ButtonProps> = ({ text, style, icon, iconStyle, variant = 'default' }) => {
+export const Button: FC<ButtonProps> = ({
+  text,
+  style,
+  icon,
+  iconStyle = {},
+  variant = 'default',
+}) => {
   return (
     <button
       className={variant === 'smooth' ? styles.buttonSmooth : styles.button}
       style={style}>
-      {icon && (
+      {/* {icon && (
         <div
           className={styles.icon}
           style={{ backgroundImage: `url(../../../assets/${icon})`, ...iconStyle }}></div>
+      )} */}
+      {icon && (
+        <SvgIcon
+          iconName={icon}
+          svgProp={iconStyle}
+        />
       )}
       <p
         className={styles.text}
